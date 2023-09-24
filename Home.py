e41598921886844f701dbaa900bc139cd8d8dc9e
ashnair5335 <109771334+ashnair5335@users.noreply.github.com>
2023-09-24 10:50:34 -0500

    Added title and short description

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Scribe: A College Essay Helper")
+
+st.write("Welcome to Scribe, a college essay writing helper that contains many resources to guide you on the \
+         path to successfully writing your college essays and helping you on your way to college.")
+
+st.write("Explore our database of resources to assist you on your essay writing like videos, examples, and \
+         written tutorials. Also try out our Chat-GPT Admissions Counselor for Essays (CHACE) for realistic \
+         feedback on your written essays so you can see what to improve on!")
